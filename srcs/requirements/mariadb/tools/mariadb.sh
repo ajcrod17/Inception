3f3c122b264b47af1b94bac54828022f29465ec4
Alexandre Rodrigues <acaldeir@student.42lisboa.com>
2026-07-05 16:30:42 +0100

    MariaDB small adjustments including comments

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -6,7 +6,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
     echo "Starting MariaDB in background to create users..."
+    # Starts MariaDB server, the ampersand (&) sends it to the background
     mysqld --user=mysql &
+    # Store the process ID of the background server to be able to kill it later
     pid="$!"
 
     # Wait for the database to start
@@ -19,17 +21,24 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     DB_ROOT_PASS=$(cat /run/secrets/db_root_password | tr -d '\n')
     
     # Execute SQL commands to set up the database and users
+    # -e "...": Tells the mysql client CLI to execute the string inside the quotes as a direct SQL query
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    # The % wildcard allows MYSQL_USER to log in from any host — WordPress container will be allowed
+    # to connect over the Docker network
     mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
     mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
+    # Tells MariaDB to reload its internal grant tables so all these new changes take effect
     mysql -u root -p"${DB_ROOT_PASS}" -e "FLUSH PRIVILEGES;"
 
     echo "Shutting down background MariaDB..."
     mysqladmin -u root -p"${DB_ROOT_PASS}" shutdown
+    # wait until the background process completely finishes shutting down
     wait "$pid"
 fi
 
 echo "Starting MariaDB..."
 # Start MariaDB in the foreground
+# exec: Replaces the entire Bash script process with the target command. The script process completely dies,
+# and mysqld_safe takes over PID 1 inside the container. Automatically restarts the database server if it crashes
 exec mysqld_safe --user=mysql
